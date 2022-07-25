79635565af2248cef0703d6062e138e295d56f28
kouyo <46226504+kouyo-quotient@users.noreply.github.com>
2022-07-25 08:12:55 -0600

    Create ArreglosLoopBasico.java

---
+++ java/Arreglos/ArreglosLoopBasico.java
@@ -0,0 +1,10 @@
+public class ArreglosLoopBasico {
+
+    public static void main(String[] args) {
+        String[] nombres = {"Alejandro","Maria","Luis","Juan","Luisa"};
+
+        for(int i=0;i<nombres.length;i++){
+            System.out.println(nombres[i]);
+        }
+    }
+}
