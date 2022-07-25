6893555a2ebf1fb568135a830208ddabcd0e1acc
kouyo <46226504+kouyo-quotient@users.noreply.github.com>
2022-07-25 08:13:17 -0600

    Create ArreglosLoopEnchanced.java

---
+++ java/Arreglos/ArreglosLoopEnchanced.java
@@ -0,0 +1,10 @@
+public class ArreglosLoopEnchanced {
+
+    public static void main(String[] args) {
+        String[] nombres = {"Alejandro","Maria","Luis","Juan","Luisa"};
+
+        for(String i:nombres){
+            System.out.println(i);
+        }
+    }
+}
