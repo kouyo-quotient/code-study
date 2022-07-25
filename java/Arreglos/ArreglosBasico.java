02eb597f3d17ed82bcccb1c325113af46c5e9de5
kouyo <46226504+kouyo-quotient@users.noreply.github.com>
2022-07-25 08:10:07 -0600

    Create ArreglosBasico.java

---
+++ java/Arreglos/ArreglosBasico.java
@@ -0,0 +1,14 @@
+public class ArreglosBasico {
+
+    public static void main(String[] args) {
+        int[] numeros = new int[3];
+
+        numeros[0] = 1;
+        numeros[1] = 2;
+        numeros[2] = 3;
+
+        for(int i=0;i<3;i++){
+            System.out.println(numeros[i]);
+        }
+    }
+}
