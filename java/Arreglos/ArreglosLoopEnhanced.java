f88e08cc6ea92a2cc1ca5fcac90dbb5bcc6a80e2
kouyo <46226504+kouyo-quotient@users.noreply.github.com>
2022-07-26 15:40:51 -0600

    Update and rename ArreglosLoopEnchanced.java to ArreglosLoopEnhanced.java

--- java/Arreglos/ArreglosLoopEnhanced.java
+++ java/Arreglos/ArreglosLoopEnhanced.java
@@ -1,4 +1,4 @@
-public class ArreglosLoopEnchanced {
+public class ArreglosLoopEnhanced {
 
     public static void main(String[] args) {
         String[] nombres = {"Alejandro","Maria","Luis","Juan","Luisa"};
