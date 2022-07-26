01ea99ee19baa58022b67a9ef90907ff4d433ae7
kouyo <46226504+kouyo-quotient@users.noreply.github.com>
2022-07-26 16:18:46 -0600

    Create Metodos.java

---
+++ java/Otros/Metodos.java
@@ -0,0 +1,12 @@
+public class Metodos {
+
+    public static void main(String[] args) {
+        String ejemplo = "Ejemplo";
+        hello(ejemplo);
+    }
+
+    // El valor dentro de String en este metodo puede tener cualquier nombre
+    static void hello(String ejemplo){
+        System.out.println("Texto de "+ejemplo);
+    }
+}
